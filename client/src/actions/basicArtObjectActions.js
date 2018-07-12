1739d9bd1c26e08bb6c0bf6665dea9350654176e
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-07-12 17:36:56 -0400

    Random Art Button now ignores records if there is no image url
    ...and retreives new record.

--- client/src/actions/basicArtObjectActions.js
+++ client/src/actions/basicArtObjectActions.js
@@ -7,18 +7,27 @@ const paramsString = '?apikey=3ff0e030-8144-11e8-b372-95bc18ef563e&size=1&sort=r
 
 export function mainRandomButtonClicked() {
 
-  console.log("You clicked the button, son!", process.env)
+  // console.log("You clicked the button, son!", process.env)
 
 
   fetch(baseURL + paramsString)
   .then(response => response.json())
-  .then(response => console.log(response))
+  .then(response => response.records[0])
+  .then(record => {
+    debugger
+    if (!record.primaryimageurl) {
+      console.log("Retreived invalid record")
+      mainRandomButtonClicked()
+    }
+    else {
+      console.log("Retreived valid record")
+      console.log("Here's the url:", record.primaryimageurl)
+    }
+  })
+
 
   // return function(dispatch) {
-  //   return fetch(baseURL, {
-  //     method: 'GET',
-  //     body: 'apikey=3ff0e030-8144-11e8-b372-95bc18ef563e&size=1&sort=random&classification=26|21&q=+description:*%20+labeltext:*'
-  //   })
+  //   return fetch(baseURL + paramsString)
   //   .then(response => response.json())
   //   .then(response => console.log(response))
   // }
