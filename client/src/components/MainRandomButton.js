f945f34da220463f90bb217724ba8c210c68baa1
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-07-14 13:51:34 -0400

    Browse Random Art Button renders information and picture on page.
    Big step.

--- client/src/components/MainRandomButton.js
+++ client/src/components/MainRandomButton.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { bindActionCreators} from 'redux'
+import { bindActionCreators } from 'redux'
 
-import {mainRandomButtonClicked} from '../actions/basicArtObjectActions'
+import { mainRandomButtonClicked } from '../actions/basicArtObjectActions'
 
 import { Button } from 'semantic-ui-react'
 
