8a6e437773c12c8b3355dcb7cc0057f0fb2b2045
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-07-17 17:34:26 -0400

    Remove "whole grid" from Top and NavBar components.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,17 +3,12 @@ import { NavLink, Link } from 'react-router-dom'
 import { Grid, Button } from 'semantic-ui-react'
 
 const NavBar = () => (
-    <Grid >
-      <Grid.Row>
-        <Grid.Column>
-          <Button as={NavLink} to='/' exact basic activeStyle={{basic: false}}> Home </Button>
-          <Button as={NavLink} to='/art' exact basic activeStyle={{basic: false}}> Browse Art </Button>
-          <Button as={NavLink} to='/favorites' exact basic activeStyle={{basic: false}}> Favorites </Button>
-          <Button as={NavLink} to='/history' exact basic activeStyle={{basic: false}}> Browsing History </Button>
-        </ Grid.Column>
-      </ Grid.Row>
-    </ Grid>
-
+    <div>
+      <Button as={NavLink} to='/' exact basic activeStyle={{basic: false}}> Home </Button>
+      <Button as={NavLink} to='/art' exact basic activeStyle={{basic: false}}> Browse Art </Button>
+      <Button as={NavLink} to='/favorites' exact basic activeStyle={{basic: false}}> Favorites </Button>
+      <Button as={NavLink} to='/history' exact basic activeStyle={{basic: false}}> Browsing History </Button>
+    </div>
   )
 
 
--- client/src/components/Top.js
+++ client/src/components/Top.js
@@ -4,13 +4,8 @@ import { Header, Grid } from 'semantic-ui-react'
 
 const Top = () => (
 
-  <Grid>
-    <Grid.Row>
-      <Grid.Column width={16}>
-        <Header as='h1'> Browseum </Header>
-      </ Grid.Column>
-    </Grid.Row>
-  </Grid>
+  <Header as='h1'> Browseum </Header>
+
 
 )
 
