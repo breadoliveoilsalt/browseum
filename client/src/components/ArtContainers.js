87806925f3a55ce1ae3541e191c126d7a12a7e70
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-07-17 14:55:38 -0400

    Pass mainRandomButtonClicked() to button component from ArtContainers BUT...
    ...this is the long version.  To shorten MainRandomButton to make it
    a presentational component.

--- client/src/components/ArtContainers.js
+++ client/src/components/ArtContainers.js
@@ -1,38 +1,31 @@
 import React, { Component } from 'react'
+
+
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+import { mainRandomButtonClicked } from '../actions/basicArtObjectActions'
+
 import { Grid } from 'semantic-ui-react'
+
 import MainRandomButton from './MainRandomButton'
 
 class ArtContainers extends Component {
 
-  // DOES NOT WORK; MESSES WITH THE FORMATTING.
-  // THE SMALLEST COMPONENT IS THE ONE THAT HAS TO HAVE THE GRID.ROW
-  // YOU CAN HAVE ONE GRID ROW WITH MANY COLUMNS.
-  // ANOTHER WAY TO PUT IT: EACH GRID ROW HAS TO BE ITS OWN COMPONENT.
-  // <div>
-  //   <Grid.Row>
-  //     This is the first row
-  //   </Grid.Row>
-  //
-  //   <Grid.Row>
-  //     <Grid.Column width={10}>
-  //       You made it to Art. This is column 1.
-  //     </ Grid.Column>
-  //
-  //     <Grid.Column width={6}>
-  //       This is column 2.
-  //     </ Grid.Column>
-  //
-  //   </ Grid.Row>
-  // </div>
-
   render() {
+
+
     return (
 
-      <MainRandomButton />
+      <MainRandomButton mainRandomButtonClicked={this.props.mainRandomButtonClicked}/>
 
     )
   }
 
 }
 
-export default ArtContainers
+const mapDispatchToProps = (dispatch) => {
+  return { mainRandomButtonClicked: bindActionCreators(mainRandomButtonClicked, dispatch)}
+}
+
+export default connect(null, mapDispatchToProps)(ArtContainers)
--- client/src/components/MainRandomButton.js
+++ client/src/components/MainRandomButton.js
@@ -1,28 +1,21 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-
-import { mainRandomButtonClicked } from '../actions/basicArtObjectActions'
 
 import { Button, Grid } from 'semantic-ui-react'
 
-
-
 class MainRandomButton extends Component {
 
-  render(){
-    return(
-      <Grid.Row centered>
-        <Grid.Column width={6}>
-          <Button fluid onClick={this.props.mainRandomButtonClicked}> Browse Random Art! </Button>
-        </ Grid.Column>
-      </Grid.Row>
-    )
-  }
+    render() {
+
+      console.log("Props:", this.props)
+
+      return (
+        <Grid.Row centered>
+          <Grid.Column width={6}>
+            <Button fluid onClick={this.props.mainRandomButtonClicked}> Get New Art! </Button>
+          </ Grid.Column>
+        </Grid.Row>
+      )
+}
 }
 
-const mapDispatchToProps = (dispatch) => {
-  return { mainRandomButtonClicked: bindActionCreators(mainRandomButtonClicked, dispatch)}
-}
-
-export default connect(null, mapDispatchToProps)(MainRandomButton)
+export default MainRandomButton
