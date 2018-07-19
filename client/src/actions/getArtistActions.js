bfe6afb2628fac7a56fe8e2fd781acc553481928
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-07-19 18:17:16 -0400

    getArtistButtonClicked now filters results according to whether object has primaryimageurl

--- client/src/actions/getArtistActions.js
+++ client/src/actions/getArtistActions.js
@@ -9,15 +9,26 @@ export function getArtistButtonClicked(currentArtObject){
 
   console.log("artist id", artistAPIId)
 
-  const url = `https://api.harvardartmuseums.org/object?apikey=3ff0e030-8144-11e8-b372-95bc18ef563e&person=${artistAPIId}&hasimage=1&size=100`
+  // This is what I had as of 180719 6pm - remember to change back
+  // const url = `https://api.harvardartmuseums.org/object?apikey=3ff0e030-8144-11e8-b372-95bc18ef563e&person=${artistAPIId}&hasimage=1&size=100`
+
+  // Don't forget to sort randomly!
+
+    // For the sake of testing, setting this to Picaso's id.
+  const url = ` https://api.harvardartmuseums.org/object?apikey=3ff0e030-8144-11e8-b372-95bc18ef563e&person=28064&hasimage=1&size=100`
 
   return function(dispatch) {
     return fetch(url)
       .then(response => response.json())
       .then(response => filterRecords(response.records))
+      .then(filteredRecords => console.log("Here are the filtered records: ", filteredRecords))
   }
 }
 
 function filterRecords(records) {
   console.log("You are filtering the records now.")
+  console.log("Here are the records: ", records)
+  return records.filter( record => {
+    return record.primaryimageurl
+  })
 }
