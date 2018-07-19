57991e5106a5975b422daf734f0420b2de8228f5
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-07-19 17:56:54 -0400

    Debug getArtistButtonClicked so that currentArtObject is passed to it successfully... ...and successful, basic API call is made.
    Now to work on filtering the results.

--- client/src/actions/getArtistActions.js
+++ client/src/actions/getArtistActions.js
@@ -5,7 +5,9 @@ export function getArtistButtonClicked(currentArtObject){
 
   console.log("currentArtObject:", currentArtObject)
 
-  const artistAPIId = 0
+  const artistAPIId = currentArtObject.artistAPIId
+
+  console.log("artist id", artistAPIId)
 
   return function(dispatch) {
     return fetch(`https://api.harvardartmuseums.org/object?apikey=3ff0e030-8144-11e8-b372-95bc18ef563e&person=${artistAPIId}&hasimage=1&size=100`)
