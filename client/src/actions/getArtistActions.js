246b4950db225430469dd6ab4a5df4559baeb3ce
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-07-25 14:27:11 -0400

    GOOD FUTURE STARTING POINT -- Return fetchBasicData to truly random. Remove comments; general clean up.
    And again, getArtist is working, returning only new art.

--- client/src/actions/getArtistActions.js
+++ client/src/actions/getArtistActions.js
@@ -1,11 +1,6 @@
 import fetch from 'isomorphic-fetch'
 import * as helpers from './helperActions'
 
-    // Don't forget to sort randomly!
-
-    //   // For the sake of testing, setting this to Picaso's id.
-    // const url = ` https://api.harvardartmuseums.org/object?apikey=3ff0e030-8144-11e8-b372-95bc18ef563e&person=28064&hasimage=1&size=100`
-
 export function getArtistButtonClicked(currentArtObject){
 
   return function(dispatch, getState) {
@@ -14,17 +9,12 @@ export function getArtistButtonClicked(currentArtObject){
     const artistApiId = state.currentArtObject.artistApiId
     const objectApiId = state.currentArtObject.objectApiId
 
-
-    // This is what I had as of 180719 6pm - remember to change back
     const url = `https://api.harvardartmuseums.org/object?apikey=3ff0e030-8144-11e8-b372-95bc18ef563e&person=${artistApiId}&hasimage=1&size=100`
 
-// records below should be object, as in art object
-// UP TO HERE -- HAVE TO TEST IF THIS WORKS
     return fetch(url)
       .then(response => response.json())
       .then(response => filterRecordsWithImages(response.records, objectApiId))
       .then(filteredRecords => findAnOriginalRecord(filteredRecords, state))
-      // .then(foundRecord => console.log("here is the found record: ", foundRecord))
       .then(record => {
         if (!record) {
           throw {errorType: "NO_ARTIST_RECORDS"}
@@ -52,14 +42,14 @@ function findAnOriginalRecord(filteredRecords, state) {
   let arrayOfHistoryIds = []
   let newRecord
 
-  // Make an array of the object ids in the sessionHistory
+    // Make an array of the object ids in the sessionHistory
   for (var i = 0; i < sessionHistory.length; i++) {
     arrayOfHistoryIds.push(sessionHistory[i].objectApiId)
   }
 
-  // Go through the filteredRecords. Return the first that is not in sessionHistory,
-  // i.e., it hasn't been viewed yet, by comparing ids. If all the filteredRecords are in the sessionHistory,
-  // this function returns newRecord as undefined, and an error is thrown in getArtistButtonClicked
+    // Go through the filteredRecords. Return the first that is not in sessionHistory,
+    // i.e., it hasn't been viewed yet, by comparing ids. If all the filteredRecords are in the sessionHistory,
+    // this function returns newRecord as undefined, and an error is thrown in getArtistButtonClicked
   for (var i = 0; i < filteredRecords.length; i++) {
     if (arrayOfHistoryIds.includes(filteredRecords[i].objectid)) {
       continue
