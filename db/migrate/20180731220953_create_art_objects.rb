80e409bdaca805a5372dd5c363b6c74503828a9c
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-07-31 18:56:22 -0400

    Create migration file for Art_Object model and run rake db:migrate. Got warning:
    Rails Error: Unable to access log file. Please ensure that /Users/adistinti/Desktop/final-project/log/development.log exists and is writable (ie, make it writable for user and group: chmod 0664 /Users/adistinti/Desktop/final-project/log/development.log). The log level has been raised to WARN and the output directed to STDERR until the problem is fixed.

--- db/migrate/20180731220953_create_art_objects.rb
+++ db/migrate/20180731220953_create_art_objects.rb
@@ -1,8 +1,22 @@
 class CreateArtObjects < ActiveRecord::Migration[5.2]
   def change
     create_table :art_objects do |t|
-
-      t.timestamps
+      t.integer :object_Api_Id
+      t.string :primary_image_url
+      t.string :title
+      t.string :artist
+      t.integer :artist_Api_Id
+      t.string :medium
+      t.string :dated
+      t.string :century
+      t.string :culture
+      t.string :label_text
+      t.string :description
+      t.string :commentary
+      t.datetime :first_Viewed
+      t.datetime :last_Viewed
+      t.boolean :favorite
+      t.timestamps null: false
     end
   end
 end
