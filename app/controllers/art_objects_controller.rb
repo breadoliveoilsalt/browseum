e4aab4d5767533625481903c6c675a91fd6e007a
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-08-01 17:36:11 -0400

    Return sample JSON from POST to /api/artobjects... ...prints out a console message from the server in response. Good stuff.

--- app/controllers/art_objects_controller.rb
+++ app/controllers/art_objects_controller.rb
@@ -3,7 +3,6 @@ class ArtObjectsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    binding.pry
     render :json => {"text": "You are in controller#create"}
   end
 
