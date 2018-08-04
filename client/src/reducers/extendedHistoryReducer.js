79cb3dfe0282ef7bb8cb358bee334cb14ac341c9
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-08-04 16:15:39 -0400

    Fix error in extendedHistoryReducer - extendedHistory now loads in component

--- client/src/reducers/extendedHistoryReducer.js
+++ client/src/reducers/extendedHistoryReducer.js
@@ -1,7 +1,7 @@
 function extendedHistoryReducer(state = [], action) {
   switch (action.type) {
     case 'LOAD_EXTENDED_HISTORY':
-      return [...state, action.payload]
+      return [...action.payload]
     case 'RESET_EXTENDED_HISTORY':
       return []
     default:
