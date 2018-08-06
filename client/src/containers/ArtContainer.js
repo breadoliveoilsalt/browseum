35e505232da60ff360fc90882c0c460cb30ddde5
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-08-06 16:22:42 -0400

    In ArtContainer mapDispatchToProps, replace bindActionCreator format with function that wraps dispatch. Still able to getState in the thunks.

--- client/src/containers/ArtContainer.js
+++ client/src/containers/ArtContainer.js
@@ -20,10 +20,9 @@ class ArtContainer extends Component {
     }
   }
 
-  // 
-  // addToFavoritesClicked = (id, event) => {
-  //   this.props.updateSessionObjects(id, {favorite: true})}
-  // This works as of 180806 244pm
+// Upto : now change button based on whether it's a favorite or not
+
+  // This works as of 180806 334pm
   addToFavoritesClicked = (id, event) => {
     Promise.resolve(this.props.postUpdate(id, {favorite: true}))
       .then( () => this.props.updateSessionObjects(id, {favorite: true}))
@@ -46,7 +45,6 @@ class ArtContainer extends Component {
             currentArtObject={this.props.currentArtObject}
             navigationButtonClicked={this.props.navigationButtonClicked}
             addToFavoritesClicked={this.addToFavoritesClicked}
-
         />
 
       </div>
@@ -64,8 +62,8 @@ const mapStateToProps = (state) => {
 // can change all of these to () => dispatch format and then get rid of bindActionCreators
 const mapDispatchToProps = (dispatch) => {
   return {
-      getRandomArt: bindActionCreators(getRandomArt, dispatch),
-      navigationButtonClicked: bindActionCreators(navigationButtonClicked, dispatch),
+      getRandomArt: () => dispatch(getRandomArt()),
+      navigationButtonClicked: (type, errorMessage) => dispatch(navigationButtonClicked(type, errorMessage)),
       getFavorites: () => dispatch(getFavorites()),
       postUpdate: (id, data) => dispatch(postUpdate(id, data)),
       resetFavorites: (data) => dispatch(resetFavorites(data)),
