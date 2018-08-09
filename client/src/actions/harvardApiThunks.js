27599371b76b8cac6b2dc9edc2d1d1d2d4157a3f
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-08-09 17:46:48 -0400

    Important refactoring of harvardApitThunk: in getRandomArt, was able to make error removal conditional...
    ...the problem was that if an error was being thrown b/c the record
    did not have a primaryimageurl, and so it was heading to catch to
    re-execute fetchBasicData, the re-execution did not include getState!
    That was where the error was coming from.

--- client/src/actions/harvardApiThunks.js
+++ client/src/actions/harvardApiThunks.js
@@ -7,26 +7,23 @@ const baseURL = `https://api.harvardartmuseums.org/object?apikey=${apiKey}`
 
 //// MAIN FUNCTIONS FOR GET RANDOM ART BUTTON: ////
 export function getRandomArt() {
-  return function(dispatch){
+  return function(dispatch, getState){
       // Had to specify a named, separate function on next line so that fetchBasicData could call itself
       // again in the event the first record retreived from Harvard Museum API
       // did not have an image url.
-    return fetchBasicData(dispatch)
+    return fetchBasicData(dispatch, getState)
     }
   }
 
-function fetchBasicData(dispatch) {
+function fetchBasicData(dispatch, getState) {
 
-    // Calling removeError() to remove any pre-existing error before loading new Art.
-    // Note, I could not find a way to make this conditional, even with passing getState
-    // down here (which worked in naviationActions. Sometimes, getState would render and
-    // would remove error, and other times, an error would pop up in browser
-    // (TypeError: getState is not a function).  Made no sense, because
-    // this approach was clearly working some of the time.  So just have to removeError all the time, whether
-    // there is an error or not:
-  dispatch(removeError())
+  const { error } = getState()
 
-  fetch(baseURL + "&hasimage=1&sort=random&size=1")
+  if (error.errorOccurred) {
+    dispatch(removeError())
+  }
+
+  fetch(baseURL + "&hasimage=1&sort=random&size=1") // For testing: "&person=27644&sort=random&size=1")
     .then(response => response.json())
     .then(response => response.records[0])
     .then(record => {
@@ -46,7 +43,7 @@ function fetchBasicData(dispatch) {
       dispatch(addToSessionHistory(response))})
     .catch(error => {
       if (error.errorType === "INVALID_RECORD") {
-        fetchBasicData(dispatch)
+        fetchBasicData(dispatch, getState)
       } else {
         dispatch(loadError("Sorry, something seems to have gone wrong. Please click Get New Art again."))
       }
