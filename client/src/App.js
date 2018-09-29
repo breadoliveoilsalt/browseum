eac94ceccd28101df079322f98ca152615fc637a
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-09-29 11:18:45 -0400

    Clean up comments in App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,10 @@
 import React, { Component } from 'react'
-// import { BrowserRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 
-import { getFavorites } from './actions/serverApiThunks'
-
-// import Layout from './containers/Layout'
 import LayoutParent from './containers/LayoutParent'
 
 class App extends Component {
 
-  // Does not load
-  // componentDidMount() {
-  //   debugger
-  //   this.props.getFavorites()
-  // }
 
   render() {
 
@@ -24,24 +15,6 @@ class App extends Component {
 
     );
   }
-
-  // render() {
-  //
-  //   return (
-  //     <BrowserRouter>
-  //       <div className="App">
-  //         <Layout/>
-  //       </div>
-  //     </BrowserRouter>
-  //   );
-  // }
 }
 
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//       getFavorites: () => dispatch(getFavorites())
-//   }
-// }
-
-// export default connect(null, mapDispatchToProps)(App)
 export default App
