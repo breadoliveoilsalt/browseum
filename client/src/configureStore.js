e51818a494273506509aec06e425ec7ea35a45a5
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-10-30 14:05:46 -0400

    In configureStore.js, add to redux-devtools-extenion: /logOnlyInProduction
    per here: https://github.com/AymaneZizi/Weatherpro/issues/1

--- client/src/configureStore.js
+++ client/src/configureStore.js
@@ -1,6 +1,6 @@
 import { applyMiddleware, createStore } from 'redux'
 import thunkMiddleware from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction'
 
 import monitorReducersEnhancer from './enhancers/monitorReducer'
 import loggerMiddleware from './middleware/logger'
