96509a3a91a30e17a7e42cba3a9222197c990a0f
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-10-30 15:20:38 -0400

    Solve problem with deployed heroku app not communicating with Harvard API...
    ...had to set the REACT_APP_API_KEY in the heroku settings (via the
    heroku settings webpage) and had to add as a buildpack
    https://github.com/mars/create-react-app-buildpack.git
    (via the heroku settings webpage).
    
    Need to verify that all works, but it looks like it does!

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #
 
   post "/api/artobjects", to: "art_objects#create"
   get "/api/artobjects", to: "art_objects#index"
