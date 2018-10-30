a9a2149dd9898ba0dd9db6a1f6aff4fbec9dc887
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-10-30 10:05:23 -0400

    Working on production env error - add home route and home action in...
    ...the art_object controller. Running the server in production mode
    shows in the browser the same error that heroku is showing.  After some
    research this seems to be an error (perhaps) related to not having
    a "root" route in config/routes.  Running the server in production mode
    now at least gets the rails default page that this is a rails app.
    
    Command to run server in production mode:
    rails server -e production

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root "art_objects#home"
+
   post "/api/artobjects", to: "art_objects#create"
   get "/api/artobjects", to: "art_objects#index"
   put "/api/artobjects/:id", to: "art_objects#update"
