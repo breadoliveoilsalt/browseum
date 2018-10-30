80fd5047921469a71334f5203bf75d0c08e71794
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-10-30 13:27:13 -0400

    Undo changes from last two commits...
    ...the problem may be that I haven't finished the instructions with respect to
    the router. So going to run through those

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "art_objects#home"
+  # root "art_objects#home"
 
   post "/api/artobjects", to: "art_objects#create"
   get "/api/artobjects", to: "art_objects#index"
